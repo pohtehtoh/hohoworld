0b6aeb15801146a3691322e05b0612c4733073a9
MeowMeowAAAAAAA <0uwumeowmeowuwu0@gmail.com>
2023-07-10 23:55:09 +0530

    uwu

---
+++ Map1/Assets/UnityTechnologies/EffectExamples/DayNightCycle2.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class DayNightCycle2 : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float time;
+    public float fullDayLength;
+    public float startTime = 0.4f;
+    private float timeRate;
+    public Vector3 noon;
+
+    [Header("Sun")]
+    public Light sun;
+    public Gradient sunColor;
+    public AnimationCurve sunIntensity;
+
+    [Header("Moon")]
+    public Light moon;
+    public Gradient moonColor;
+    public AnimationCurve moonIntensity;
+
+    [Header("Other Lighting")]
+    public AnimationCurve lightingIntensityMultiplier;
+    public AnimationCurve reflectionIntensityMultiplier;
+
+    void Start()
+    {
+        timeRate = 1.0f / fullDayLength;
+        time = startTime;
+    }
+
+    void Update()
+    {
+        time += timeRate * Time.deltaTime;
+
+        if (time >= 1.0f)
+            time = 0.0f;
+
+        sun.transform.eulerAngles = (time - 0.25f) * noon * 4.0f;
+        moon.transform.eulerAngles = (time - 0.75f) * noon * 4.0f;
+
+        sun.intensity = sunIntensity.Evaluate(time);
+        moon.intensity = moonIntensity.Evaluate(time);
+
+        sun.color = sunColor.Evaluate(time);
+        moon.color = moonColor.Evaluate(time);
+
+        if (sun.intensity == 0 && sun.gameObject.activeInHierarchy)
+            sun.gameObject.SetActive(false);
+        else if(sun.intensity > 0 && !sun.gameObject.activeInHierarchy)
+            sun.gameObject.SetActive(true);
+
+        if (moon.intensity == 0 && moon.gameObject.activeInHierarchy)
+            moon.gameObject.SetActive(false);
+        else if (moon.intensity > 0 && !moon.gameObject.activeInHierarchy)
+            moon.gameObject.SetActive(true);
+
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+        RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
+    }
+}
